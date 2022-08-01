28ef44e66101363b861e815ce675b28666e92755
Muhammed <muhammed@sendme.ng>
2022-08-01 20:12:57 +0100

    feat: create todo-edit url

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, create_todo, todo_detail, todo_delete
+from .views import index, create_todo, todo_detail, todo_delete, todo_edit
 
 urlpatterns = [
     path('', index, name='home'),
     path('create/', create_todo, name='create-todo'),
     path('todo/<id>/', todo_detail, name='todo'),
-    path('todo-delete/<id>/', todo_delete, name="todo-delete")
+    path('todo-delete/<id>/', todo_delete, name="todo-delete"),
+    path('edit-todo/<id>/', todo_edit, name='todo-edit'),
 ]
