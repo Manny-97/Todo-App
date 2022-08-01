a62ee08a97e78aa79a2865ab48fbd675cab8810e
Muhammed <muhammed@sendme.ng>
2022-08-01 19:57:33 +0100

    feat: delete page

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, create_todo, todo_detail
+from .views import index, create_todo, todo_detail, todo_delete
 
 urlpatterns = [
     path('', index, name='home'),
     path('create/', create_todo, name='create-todo'),
     path('todo/<id>/', todo_detail, name='todo'),
+    path('todo-delete/<id>/', todo_delete, name="todo-delete")
 ]
