00d3a1504321a2cb0502c3e4fa1d1a2b6b2bc67a
Muhammed <muhammed@sendme.ng>
2022-08-01 15:27:17 +0100

    feat:setup todo table/form

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, create_todo
+from .views import index, create_todo, todo_detail
 
 urlpatterns = [
     path('', index, name='home'),
     path('create/', create_todo, name='create-todo'),
+    path('todo/<id>/', todo_detail, name='todo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,30 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from .models import Todo
+from .forms import TodoForm
 # Create your views here.
 def index(request):
     return render(request, 'todo/index.html')
 
 def create_todo(request):
-    return render(request, 'todo/create-todo.html')
+    form = TodoForm()
+    context = {'form': form}
+
+    if request.method == "POST":
+        titles = request.POST.get('title')
+        description = request.POST.get('description')
+        is_completed = request.POST.get('is_completed', False)
+
+        todo = Todo()
+
+        todo.title = title
+        todo.description = description
+        todo.is_completed = True if is_completed == "on" else False
+
+        todo.save()
+
+        return HttpResponseRedirect('')
+
+    return render(request, 'todo/create-todo.html', context)
+
+
