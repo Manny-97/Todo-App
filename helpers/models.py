4a3e01e462f5fbdf7b85110681eeb0a88d041f09
Muhammed <muhammed@sendme.ng>
2022-08-01 14:34:48 +0100

    feat: Abstract TrackingModel

---
+++ helpers/models.py
@@ -0,0 +1,12 @@
+from venv import create
+from django.db import models
+
+
+class TrackingModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
