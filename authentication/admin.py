fbc978341b18a915de02fc7f769a7fb5ab5fe4d8
Muhammed <muhammed@sendme.ng>
2022-08-03 16:42:39 +0100

    feat: register models on admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+
 from .models import User
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (['username', 'email', 'is_staff'])
+    search_fields =('username', 'email', 'is_staff')
+    list_per_page = 25
 
 admin.site.register(User, UserAdmin)
