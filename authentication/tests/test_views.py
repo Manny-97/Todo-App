230d9125d99fdc2118177aa99759969a135d25cd
Muhammed <muhammed@sendme.ng>
2022-08-03 16:19:51 +0100

    refactor: test should show register page

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -6,6 +6,10 @@ from utils.setup_test import TestSetup
 class TestViews(TestSetup):
 
     def test_should_show_register_page(self):
+        # user1 = self.create_test_user()
+        # user2 = self.create_test_two()
+
+
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'authentication/register.html')
