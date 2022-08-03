e94f35ef2ad2d42973a144d30a73a6ce9b3163ee
Muhammed <muhammed@sendme.ng>
2022-08-03 12:52:17 +0100

    feat: unit testing for todo app models

--- todo/tests/test_models.py
+++ todo/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from matplotlib.pyplot import title
 from authentication.models import User
 from django.test import TestCase
-
+from todo.models import Todo
 class TestModel(TestCase):
 
     def test_should_create_user(self):
@@ -9,6 +10,9 @@ class TestModel(TestCase):
         user.set_password('password12!')
         user.save()
 
-        self.assertEqual(str(user), 'email@app.com')
+        todo = Todo(owner=user, title='Buy milk', description='get it done')
 
+        todo.save()
+
+        self.assertEqual(str(todo), 'Buy milk')
 
